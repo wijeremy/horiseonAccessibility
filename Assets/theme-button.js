47ffcf961b8d41f57399a293b389b3422742bfac
wijeremy <jeremydavwilliams@gmail.com>
2021-05-04 12:06:16 -0500

    changed all the colors in css to variables. added a darkmode line in js that doesn't do anything. changed all instances of single classes into ids.

---
+++ Assets/theme-button.js
@@ -0,0 +1,7 @@
+document.getElementById('noImages').addEventListener('click', function () {
+    document.body.classList.toggle('noImages');
+}); 
+
+document.getElementById('darkMode').addEventListener('click', function () {
+    document.body.classList.toggle('darkMode');
+}); 
