72cba3d44c5d1498b8ff57c1c6fb55b3f866db39
wijeremy <jeremydavwilliams@gmail.com>
2021-05-05 14:10:28 -0500

    Improved performance at smaller screen size by adding media queries to shift contents around when the screen becomes small. Many of these trigger at 750 pixels, but, but there are also two media queries that trigger at 450 pixels. These last two queries change the behavior of the images and the buttons in the body of the site so they will grow smaller as the screen becomes smaller. Other media queries place the side bar at the bottom of the page and place the row of navigation links at the top into a column under the title logo. Additionally, the main body (content) images were placed under the title of each box; I thought it looked better that way. The other major change that was made was to the js and the functionality of the accessibility buttons. A new class named .darkMode was made in CSS, so that the js function for the dark mode button could call that insead of each individual variable in :root. This also allowed the default button to remove that class instead of reseting each variable.

--- Assets/theme-button.js
+++ Assets/theme-button.js
@@ -1,21 +1,13 @@
+// Images On/Off Button
 document.getElementById('noImages').addEventListener('click', function () {
     document.body.classList.toggle('noImages');
 }); 
-
+// Dark Mode On/Off Button
 document.getElementById('darkMode').addEventListener('click', function () {
-    document.documentElement.style.setProperty("--colorBackground", "#222222")
-    document.documentElement.style.setProperty("--colorHeader", "#102c3a")
-    document.documentElement.style.setProperty("--colorText", "#f3e3b6")
-    document.documentElement.style.setProperty("--colorMain1", "#003150")
-    document.documentElement.style.setProperty("--colorMain2", "#0f374d")
-    ;
+    document.body.classList.toggle('darkMode');
 }); 
-
+// Default Button
 document.getElementById('default').addEventListener('click', function () {
     document.body.classList.remove("noImages")
-    document.documentElement.style.setProperty("--colorBackground", "#d9dcd6")
-    document.documentElement.style.setProperty("--colorHeader", "#2a607c")
-    document.documentElement.style.setProperty("--colorText", "#ffffff")
-    document.documentElement.style.setProperty("--colorMain1", "#0072bb")
-    document.documentElement.style.setProperty("--colorMain2", "#2589bd");
+    document.body.classList.remove('darkMode');
 }); 
